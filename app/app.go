50d3246b0a21d41ad9a941f009c1ca5fe7ab67df
Martin Bruse <zond@google.com>
2016-10-23 22:34:23 +0200

    ..

--- app/app.go
+++ app/app.go
@@ -1,34 +1,85 @@
 package app
 
 import (
+	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"strings"
 	"sync"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/nacl/secretbox"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	oauth2service "google.golang.org/api/oauth2/v1"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
-	"google.golang.org/appengine/log"
 
 	. "github.com/zond/goaeoas"
+	oauth2service "google.golang.org/api/oauth2/v1"
 )
 
 var (
-	prod     *OAuth
-	prodLock = sync.RWMutex{}
-	router   = mux.NewRouter()
+	prodOAuth     *OAuth
+	prodOAuthLock = sync.RWMutex{}
+	prodNaCl      *NaCl
+	prodNaClLock  = sync.RWMutex{}
+	router        = mux.NewRouter()
 )
 
 type configuration struct {
 	OAuth OAuth
+}
+
+type NaCl struct {
+	Secret []byte
+}
+
+func getNaClKey(ctx context.Context) *datastore.Key {
+	return datastore.NewKey(ctx, "NaCl", "prod", 0, nil)
+}
+
+func getNaCl(ctx context.Context) (*NaCl, error) {
+	// check if in memory
+	prodNaClLock.RLock()
+	if prodNaCl != nil {
+		defer prodNaClLock.RUnlock()
+		return prodNaCl, nil
+	}
+	prodNaClLock.RUnlock()
+	// nope, check if in datastore
+	prodNaClLock.Lock()
+	defer prodNaClLock.Unlock()
+	prodNaCl = &NaCl{}
+	if err := datastore.Get(ctx, getNaClKey(ctx), prodNaCl); err == nil {
+		return prodNaCl, nil
+	} else if err != datastore.ErrNoSuchEntity {
+		return nil, err
+	}
+	// nope, create new key
+	prodNaCl.Secret = make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, prodNaCl.Secret); err != nil {
+		return nil, err
+	}
+	// write it transactionally into datastore
+	if err := datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+		if err := datastore.Get(ctx, getNaClKey(ctx), prodNaCl); err == nil {
+			return nil
+		} else if err != datastore.ErrNoSuchEntity {
+			return err
+		}
+		if _, err := datastore.Put(ctx, getNaClKey(ctx), prodNaCl); err != nil {
+			return err
+		}
+		return nil
+	}, &datastore.TransactionOptions{XG: false}); err != nil {
+		return nil, err
+	}
+	return prodNaCl, nil
 }
 
 type OAuth struct {
@@ -41,19 +92,19 @@ func getOAuthKey(ctx context.Context) *datastore.Key {
 }
 
 func getOAuth(ctx context.Context) (*OAuth, error) {
-	prodLock.RLock()
-	if prod != nil {
-		prodLock.RUnlock()
-		return prod, nil
+	prodOAuthLock.RLock()
+	if prodOAuth != nil {
+		defer prodOAuthLock.RUnlock()
+		return prodOAuth, nil
 	}
-	prodLock.RUnlock()
-	prodLock.Lock()
-	defer prodLock.Unlock()
-	prod = &OAuth{}
-	if err := datastore.Get(ctx, getOAuthKey(ctx), prod); err != nil {
+	prodOAuthLock.RUnlock()
+	prodOAuthLock.Lock()
+	defer prodOAuthLock.Unlock()
+	prodOAuth = &OAuth{}
+	if err := datastore.Get(ctx, getOAuthKey(ctx), prodOAuth); err != nil {
 		return nil, err
 	}
-	return prod, nil
+	return prodOAuth, nil
 }
 
 func getOAuth2Config(ctx context.Context, r Request) (*oauth2.Config, error) {
@@ -103,7 +154,14 @@ func handleIndex(w ResponseWriter, r Request) error {
 		[]string{
 			"Usage",
 			"Use the `Accept` header or `accept` query parameter to choose `text/html` or `application/json` as output.",
+			"Use the `login` link to log in to the system.",
 			"CORS requests are allowed.",
+		},
+		[]string{
+			"Authentication",
+			"The `login` link redirects to the Google OAuth2 login flow, and then back the `redirect-to` query param used when `GET`ing the `login` link.",
+			"In the final redirect, the query parameter `token` will be your OAuth2 token.",
+			"Use this `token` parameter when loading requests, or base64 decode it and use the `access_token` field inside as `Authorization: Bearer ...` header to authenticate requests.",
 		},
 	}).AddLink(r.NewLink(Link{
 		Rel:   "self",
@@ -144,6 +202,25 @@ func handleLogin(w ResponseWriter, r Request) error {
 	return nil
 }
 
+func encodeToken(ctx context.Context, userInfo *oauth2service.Userinfoplus) (string, error) {
+	nacl, err := getNaCl(ctx)
+	if err != nil {
+		return "", err
+	}
+	plain, err := json.Marshal(userInfo)
+	if err != nil {
+		return "", err
+	}
+	var nonceAry [24]byte
+	if _, err := io.ReadFull(rand.Reader, nonceAry[:]); err != nil {
+		return "", err
+	}
+	var secretAry [32]byte
+	copy(secretAry[:], nacl.Secret)
+	cipher := secretbox.Seal(nonceAry[:], plain, &nonceAry, &secretAry)
+	return base64.URLEncoding.EncodeToString(cipher), nil
+}
+
 func handleOAuth2Callback(w ResponseWriter, r Request) error {
 	ctx := appengine.NewContext(r.Req())
 
@@ -157,12 +234,20 @@ func handleOAuth2Callback(w ResponseWriter, r Request) error {
 		return err
 	}
 
-	b, err := json.Marshal(token)
+	client := oauth2.NewClient(ctx, oauth2.StaticTokenSource(token))
+	service, err := oauth2service.New(client)
 	if err != nil {
 		return err
 	}
-	b64 := base64.URLEncoding.EncodeToString(b)
-	log.Infof(ctx, "b64: %s", b64)
+	userInfo, err := oauth2service.NewUserinfoService(service).Get().Context(ctx).Do()
+	if err != nil {
+		return err
+	}
+
+	userToken, err := encodeToken(ctx, userInfo)
+	if err != nil {
+		return err
+	}
 
 	redirectURL, err := url.Parse(r.Req().URL.Query().Get("state"))
 	if err != nil {
@@ -170,7 +255,7 @@ func handleOAuth2Callback(w ResponseWriter, r Request) error {
 	}
 
 	query := url.Values{}
-	query.Set("token", b64)
+	query.Set("token", userToken)
 	redirectURL.RawQuery = query.Encode()
 
 	http.Redirect(w, r.Req(), redirectURL.String(), 303)
@@ -209,20 +294,9 @@ func handleConfigure(w ResponseWriter, r Request) error {
 	return nil
 }
 
-func oauth2Filter(w ResponseWriter, r Request) error {
-	var token *oauth2.Token
-
-	tokenParam := r.Req().URL.Query().Get("token")
-	if tokenParam != "" {
-		b, err := base64.URLEncoding.DecodeString(tokenParam)
-		if err != nil {
-			return err
-		}
-		token = &oauth2.Token{}
-		if err := json.Unmarshal(b, token); err != nil {
-			return err
-		}
-	} else {
+func tokenFilter(w ResponseWriter, r Request) error {
+	token := r.Req().URL.Query().Get("token")
+	if token == "" {
 		if authHeader := r.Req().Header.Get("Authorization"); authHeader != "" {
 			parts := strings.Split(authHeader, " ")
 			if len(parts) != 2 {
@@ -231,24 +305,48 @@ func oauth2Filter(w ResponseWriter, r Request) error {
 			if strings.ToLower(parts[0]) != "bearer" {
 				return fmt.Errorf("Authorization header part 1 not 'bearer'")
 			}
-			token = &oauth2.Token{
-				AccessToken: parts[1],
-			}
+			token = parts[1]
 		}
 	}
 
-	if token != nil {
+	if token != "" {
 		ctx := appengine.NewContext(r.Req())
-		client := oauth2.NewClient(ctx, oauth2.StaticTokenSource(token))
-		service, err := oauth2service.New(client)
+
+		b, err := base64.URLEncoding.DecodeString(token)
 		if err != nil {
 			return err
 		}
-		userInfo, err := oauth2service.NewUserinfoService(service).Get().Context(ctx).Do()
+
+		var nonceAry [24]byte
+		copy(nonceAry[:], b)
+		nacl, err := getNaCl(ctx)
 		if err != nil {
+			return err
+		}
+		var secretAry [32]byte
+		copy(secretAry[:], nacl.Secret)
+
+		plain, ok := secretbox.Open([]byte{}, b[24:], &nonceAry, &secretAry)
+		if !ok {
+			http.Error(w, "badly decrypted token", 403)
+			return nil
+		}
+
+		userInfo := &oauth2service.Userinfoplus{}
+		if err := json.Unmarshal(plain, userInfo); err != nil {
 			return err
 		}
 		r.Values()["user"] = userInfo
+		if r.Media() == "text/html" {
+			r.DecorateLinks(func(l *Link, u *url.URL) error {
+				if l.Rel != "logout" {
+					q := u.Query()
+					q.Set("token", token)
+					u.RawQuery = q.Encode()
+				}
+				return nil
+			})
+		}
 	}
 	return nil
 }
@@ -261,6 +359,6 @@ func init() {
 	Handle(router, "/logout", []string{"GET"}, "logout", handleLogout)
 	Handle(router, "/redirect", []string{"GET"}, "redirect", handleRedirect)
 	Handle(router, "/oauth2callback", []string{"GET"}, "oauth2callback", handleOAuth2Callback)
-	AddFilter(oauth2Filter)
+	AddFilter(tokenFilter)
 	http.Handle("/", router)
 }
