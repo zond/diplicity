4056ebf0c39e7f6c45193e9c0560d62b2f9207a8
Martin Bruse <zond@google.com>
2016-11-04 21:51:59 +0100

    Testing that outsiders can see public channels, but only public channels.

--- diptest/chat_test.go
+++ diptest/chat_test.go
@@ -19,7 +19,6 @@ func testChat(t *testing.T) {
 
 		nations[i] = games[i].
 			Find([]string{"Properties", "Members"}, []string{"User", "Id"}, startedGameEnvs[i].GetUID()).GetValue("Nation").(string)
-
 	}
 
 	games[0].Follow("channels", "Links").Success().AssertEmpty("Properties")
@@ -53,4 +52,35 @@ func testChat(t *testing.T) {
 		Find([]string{"Properties"}, []string{"Name"}, chanName).
 		Follow("messages", "Links").Success().
 		Find([]string{"Properties"}, []string{"Properties", "Body"}, msg1)
+
+	outsiderGame := NewEnv().SetUID(String("fake")).GetRoute(game.IndexRoute).Success().
+		Follow("started-games", "Links").Success().
+		Find([]string{"Properties"}, []string{"Properties", "Desc"}, startedGameDesc)
+
+	outsiderGame.Follow("channels", "Links").Success().
+		AssertEmpty("Properties")
+
+	msg2 := String("message")
+
+	games[0].Follow("channels", "Links").Success().
+		Follow("message", "Links").Body(map[string]interface{}{
+		"Body":           msg2,
+		"ChannelMembers": nations,
+	}).Success()
+
+	sortedNations := make([]string, len(nations))
+	copy(sortedNations, nations)
+	sort.Sort(sort.StringSlice(sortedNations))
+
+	for i := range startedGameEnvs {
+		games[i].Follow("channels", "Links").Success().
+			Find([]string{"Properties"}, []string{"Name"}, strings.Join(sortedNations, ",")).
+			Follow("messages", "Links").Success().
+			Find([]string{"Properties"}, []string{"Properties", "Body"}, msg2)
+	}
+
+	outsiderGame.Follow("channels", "Links").Success().
+		Find([]string{"Properties"}, []string{"Name"}, strings.Join(sortedNations, ",")).
+		Follow("messages", "Links").Success().
+		Find([]string{"Properties"}, []string{"Properties", "Body"}, msg2)
 }
