7536da281153183bc568dff1cd4c2c31029108fd
Martin Bruse <zond@google.com>
2016-11-10 10:40:44 +0100

    Added test for game states.

---
+++ diptest/game_state_test.go
@@ -0,0 +1,33 @@
+package diptest
+
+import "testing"
+
+func testGameState(t *testing.T) {
+	g0 := startedGames[0]
+	g1 := startedGames[1]
+	nat0 := startedGameNats[0]
+	nat1 := startedGameNats[1]
+
+	g0.Follow("game-states").Success().AssertLen(7, "Properties").
+		Find([]string{"Properties"}, []string{"Properties", "Nation"}, nat0).
+		Follow("update", "Links").Body(map[string]interface{}{
+		"Muted": []string{nat1},
+	}).Success()
+
+	g0.Follow("game-states").Success().AssertLen(7, "Properties").
+		Find([]string{"Properties"}, []string{"Properties", "Nation"}, nat0).
+		AssertEq([]string{nat1}, "Properties", "Muted")
+
+	g0.Follow("game-states").Success().AssertLen(7, "Properties").
+		Find([]string{"Properties"}, []string{"Properties", "Nation"}, nat1).
+		AssertNil("Properties", "Muted")
+
+	g1.Follow("game-states").Success().AssertLen(7, "Properties").
+		Find([]string{"Properties"}, []string{"Properties", "Nation"}, nat0).
+		AssertEq([]string{nat1}, "Properties", "Muted")
+
+	g1.Follow("game-states").Success().AssertLen(7, "Properties").
+		Find([]string{"Properties"}, []string{"Properties", "Nation"}, nat1).
+		AssertNil("Properties", "Muted")
+
+}
