624f367775291482455bc5eb240474109837536a
Martin Bruse <zond@google.com>
2016-11-10 09:28:26 +0100

    Made the root page test a bit more scaleable in regards to number of links to test for.

--- diptest/root_test.go
+++ diptest/root_test.go
@@ -6,28 +6,55 @@ import (
 	"github.com/zond/diplicity/game"
 )
 
+var loggedInRels = []string{
+	"logout",
+	"my-staging-games",
+	"my-started-games",
+	"my-finished-games",
+	"started-games",
+	"finished-games",
+	"open-games",
+	"create-game",
+}
+
+var loggedOutRels = []string{
+	"login",
+}
+
+var bothRels = []string{
+	"self",
+	"variants",
+}
+
 func TestRootNotLoggedIn(t *testing.T) {
-	NewEnv().GetRoute(game.IndexRoute).Success().
+	r := NewEnv().GetRoute(game.IndexRoute).Success().
 		AssertEq("diplicity", "Name").
 		AssertEq("Diplicity", "Type").
-		AssertNil("Properties", "User").
-		AssertRel("login", "Links").
-		AssertRel("self", "Links")
+		AssertNil("Properties", "User")
+	for _, rel := range loggedInRels {
+		r.AssertNotRel(rel, "Links")
+	}
+	for _, rel := range loggedOutRels {
+		r.AssertRel(rel, "Links")
+	}
+	for _, rel := range bothRels {
+		r.AssertRel(rel, "Links")
+	}
 }
 
 func TestRootLoggedIn(t *testing.T) {
 	uid := String("fake")
-	NewEnv().SetUID(uid).GetRoute(game.IndexRoute).Success().
+	r := NewEnv().SetUID(uid).GetRoute(game.IndexRoute).Success().
 		AssertEq("diplicity", "Name").
 		AssertEq("Diplicity", "Type").
-		AssertEq(uid, "Properties", "User", "Id").
-		AssertRel("logout", "Links").
-		AssertRel("my-staging-games", "Links").
-		AssertRel("my-started-games", "Links").
-		AssertRel("my-finished-games", "Links").
-		AssertRel("open-games", "Links").
-		AssertRel("started-games", "Links").
-		AssertRel("finished-games", "Links").
-		AssertRel("create-game", "Links").
-		AssertRel("self", "Links")
+		AssertEq(uid, "Properties", "User", "Id")
+	for _, rel := range loggedInRels {
+		r.AssertRel(rel, "Links")
+	}
+	for _, rel := range loggedOutRels {
+		r.AssertNotRel(rel, "Links")
+	}
+	for _, rel := range bothRels {
+		r.AssertRel(rel, "Links")
+	}
 }
