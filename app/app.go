1214d36716b92c8e472165c99a9d5a0a98e6cb4f
Martin Bruse <zond@google.com>
2016-11-16 14:15:08 +0100

    Added jsonform and jsv as separate scripts by telling goaeoas about them.

--- app/app.go
+++ app/app.go
@@ -2,12 +2,25 @@ package app
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/gorilla/mux"
 	"github.com/zond/diplicity/routes"
+
+	. "github.com/zond/goaeoas"
 )
 
 func init() {
+	jsonFormURL, err := url.Parse("/js/jsonform.js")
+	if err != nil {
+		panic(err)
+	}
+	SetJSONFormURL(jsonFormURL)
+	jsvURL, err := url.Parse("/js/jsv.js")
+	if err != nil {
+		panic(err)
+	}
+	SetJSVURL(jsvURL)
 	router := mux.NewRouter()
 	routes.Setup(router)
 	http.Handle("/", router)
