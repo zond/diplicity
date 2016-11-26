c175148294e44ebc00bed23ab8b7dfe806118c6b
Martin Bruse <zond@google.com>
2016-11-26 22:52:23 +0100

    Made it possible to select where to put the generated Java files.

--- java/gen.go
+++ java/gen.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"flag"
 	"os"
-	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/zond/diplicity/routes"
@@ -10,11 +10,18 @@ import (
 )
 
 func main() {
+	dir := flag.String("dir", "", "Where to put the generated files.")
+	flag.Parse()
+
+	if *dir == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	router := mux.NewRouter()
 	routes.Setup(router)
-	dir := filepath.Join("classes", "diplicity")
-	os.MkdirAll(dir, 0755)
-	if err := goaeoas.GenerateJava(dir); err != nil {
+	os.MkdirAll(*dir, 0755)
+	if err := goaeoas.GenerateJava(*dir); err != nil {
 		panic(err)
 	}
 }
