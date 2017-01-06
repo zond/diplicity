e7cc0d7d92c2d480b88e5db9cc64fba1fafc0170
Martin Bruse <zondolfin@gmail.com>
2017-01-06 20:25:20 +0100

    Made games end when only one player is active.

--- game/phase.go
+++ game/phase.go
@@ -545,7 +545,7 @@ func (p *PhaseResolver) Act() error {
 
 	// Check if the game should end.
 
-	if soloWinner != "" || len(quitters) == len(variant.Nations) {
+	if soloWinner != "" || len(quitters) > len(variant.Nations)-1 {
 		log.Infof(p.Context, "soloWinner: %q, quitters: %v => game needs to end", soloWinner, PP(quitters))
 		// Just to ensure we don't try to resolve it again, even by mistake.
 		newPhase.Resolved = true
