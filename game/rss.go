bf45e75b12988e2f453409078fd5cb7d22c30bfa
tttppp <tttppp@users.noreply.github.com>
2019-02-22 16:45:38 +0000

    Use query parameters to filter the RSS feed.

--- game/rss.go
+++ game/rss.go
@@ -1,6 +1,9 @@
 package game
 
 import (
+	"fmt"
+	"strconv"
+
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 
@@ -8,22 +11,44 @@ import (
 )
 
 type Rss struct {
-	Games []string
+	Events []string
 }
 
 func handleRss(w ResponseWriter, r Request) error {
 	ctx := appengine.NewContext(r.Req())
+	uq := r.Req().URL.Query()
 
-	rss := Rss{}
+	limit, err := strconv.ParseInt(uq.Get("limit"), 10, 64)
+	if err != nil || limit > maxLimit {
+		limit = maxLimit
+		err = nil
+	}
 
-	activeGames := Games{}
-	_, err := datastore.NewQuery(gameKind).Filter("Finished=", false).Filter("Started=", true).GetAll(ctx, &activeGames)
+	q := datastore.NewQuery(gameKind).Filter("Started=", true)
+
+	if variantFilter := uq.Get("variant"); variantFilter != "" {
+		q = q.Filter("Variant=", variantFilter)
+	}
+
+	games := Games{}
+	gameIDs, err := q.GetAll(ctx, &games)
 	if err != nil {
 		return err
 	}
+	for idx, id := range gameIDs {
+		games[idx].ID = id
+	}
 
-	for _, game := range activeGames {
-		rss.Games = append(rss.Games, game.Desc)
+	rss := Rss{}
+
+	for _, game := range games {
+		phases := []Phase{}
+		if _, err := datastore.NewQuery(phaseKind).Ancestor(game.ID).GetAll(ctx, &phases); err != nil {
+			return err
+		}
+		for _, phase := range phases {
+			rss.Events = append(rss.Events, fmt.Sprintf("%s %s %d %s %s", phase.CreatedAt, game.Desc, phase.Year, phase.Season, phase.Type))
+		}
 	}
 
 	w.SetContent(NewItem(rss).SetName("rss").SetDesc([][]string{
