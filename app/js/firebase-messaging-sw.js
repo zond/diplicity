502055a8c0a526f5a36e94e2497aa4798d688d30
Martin Bruse <zondolfin@gmail.com>
2020-03-02 13:44:50 +0100

    Backtracked on the firebase update.

--- app/js/firebase-messaging-sw.js
+++ app/js/firebase-messaging-sw.js
@@ -1,21 +1,14 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/7.9.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.9.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
-var firebaseConfig = {
-  apiKey: "AIzaSyDxQpMuCYlu95_oG7FUCLFIYIIfvKz-4D8",
-  authDomain: "diplicity-engine.firebaseapp.com",
-  databaseURL: "https://diplicity-engine.firebaseio.com",
-  projectId: "diplicity-engine",
-  storageBucket: "diplicity-engine.appspot.com",
-  messagingSenderId: "635122585664",
-  appId: "1:635122585664:web:89244768f1d41245a74fa5"
-};
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+	'messagingSenderId': 'YOUR-SENDER-ID'
+});
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,12 @@
 // Initialize Firebase
-var firebaseConfig = {
-  apiKey: "AIzaSyDxQpMuCYlu95_oG7FUCLFIYIIfvKz-4D8",
-  authDomain: "diplicity-engine.firebaseapp.com",
-  databaseURL: "https://diplicity-engine.firebaseio.com",
-  projectId: "diplicity-engine",
-  storageBucket: "diplicity-engine.appspot.com",
-  messagingSenderId: "635122585664",
-  appId: "1:635122585664:web:89244768f1d41245a74fa5"
+var config = {
+	apiKey: "AIzaSyB0rX7dts3Rk0UnDRR9A4vghO01mwCvLxY",
+	authDomain: "diplicity-engine.firebaseapp.com",
+	databaseURL: "https://diplicity-engine.firebaseio.com",
+	storageBucket: "diplicity-engine.appspot.com",
+	messagingSenderId: "635122585664"
 };
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 messaging.requestPermission().then(function() {
