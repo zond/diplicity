5f3a7a028a247fb0671a01beab31a3224bed8b07
Martin Bruse <zondolfin@gmail.com>
2020-03-05 22:17:22 +0100

    Working on workflows.

--- diptest/start_test_server.sh
+++ diptest/start_test_server.sh
@@ -3,4 +3,5 @@
 dev_appserver.py --require_indexes --skip_sdk_update_check=true --clear_datastore=true --datastore_consistency_policy=consistent ${GITHUB_WORKSPACE}/app.yaml > /dev/null 2>&1 &
 while ! curl -s http://localhost:8080/; do
   sleep 1
+  echo -n .
 done
