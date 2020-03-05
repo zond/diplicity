46556ce16d1145f208c5d59c56446d89c8130963
Martin Bruse <zondolfin@gmail.com>
2020-03-05 22:36:39 +0100

    Working on workflows.

--- diptest/start_test_server.sh
+++ diptest/start_test_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-${GCLOUD_ROOT}/bin/dev_appserver.py --help
-#dev_appserver.py --require_indexes --skip_sdk_update_check=true --clear_datastore=true --datastore_consistency_policy=consistent ${GITHUB_WORKSPACE}/app.yaml #> /dev/null 2>&1 &
-#while ! curl -s http://localhost:8080/; do
-#  sleep 1
-#  echo -n .
-#done
+echo "Starting dev_appserver.py"
+${GCLOUD_ROOT}/bin/dev_appserver.py --require_indexes --skip_sdk_update_check=true --clear_datastore=true --datastore_consistency_policy=consistent ${GITHUB_WORKSPACE}/app.yaml #> /dev/null 2>&1 &
+echo -n "Waiting for it to start serving"
+while ! curl -s http://localhost:8080/; do
+  sleep 1
+  echo -n .
+done
+echo "done"
