1b51025345e48c195d4ad386d07f2190b82eb1f0
Martin Bruse <zondolfin@gmail.com>
2020-05-30 23:35:27 +0200

    Turned off mustering for all new games.

--- game/game.go
+++ game/game.go
@@ -684,6 +684,11 @@ func createGame(w ResponseWriter, r Request) (*Game, error) {
 	}
 	game.CreatedAt = time.Now()
 
+	// TODO(zond): Remove this when we have client support for mustering properly.
+	if !appengine.IsDevAppServer {
+		game.SkipMuster = true
+	}
+
 	if !game.NoMerge && !game.Private {
 		mergedWith, err := merge(ctx, r, game, user)
 		if err != nil {
