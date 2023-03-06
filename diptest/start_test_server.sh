12727d50590ada93276daee7c25e88f815e448e5
Martin Bruse <zondolfin@gmail.com>
2023-03-06 15:35:29 +0100

    Test script work

--- diptest/start_test_server.sh
+++ diptest/start_test_server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 echo "Starting dev_appserver.py"
-${GCLOUD_ROOT}/bin/dev_appserver.py --require_indexes --skip_sdk_update_check=true --clear_datastore=true --datastore_consistency_policy=consistent ${GITHUB_WORKSPACE}/app.yaml > /dev/null 2>&1 &
-echo -n "Waiting for it to start serving"
+${GCLOUD_ROOT}/bin/dev_appserver.py --require_indexes --skip_sdk_update_check=true --clear_datastore=true --datastore_consistency_policy=consistent ${GITHUB_WORKSPACE}/app.yaml > /tmp/start_test_server.log 2>&1 &
+echo "Waiting for it to start serving"
 while ! curl -s -o /dev/null http://localhost:8080/; do
   sleep 10
   echo "Waiting ($(date))"
+  cat /tmp/start_test_server.log
 done
 echo "done"
