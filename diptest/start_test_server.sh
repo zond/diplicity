22aed5599d93d0a1275f8546443a3b10420e7d95
Martin Bruse <zondolfin@gmail.com>
2023-03-06 15:32:47 +0100

    Test script work

--- diptest/start_test_server.sh
+++ diptest/start_test_server.sh
@@ -4,7 +4,7 @@ echo "Starting dev_appserver.py"
 ${GCLOUD_ROOT}/bin/dev_appserver.py --require_indexes --skip_sdk_update_check=true --clear_datastore=true --datastore_consistency_policy=consistent ${GITHUB_WORKSPACE}/app.yaml > /dev/null 2>&1 &
 echo -n "Waiting for it to start serving"
 while ! curl -s -o /dev/null http://localhost:8080/; do
-  sleep 1
-  echo -n .
+  sleep 10
+  echo "Waiting ($(date))"
 done
 echo "done"
