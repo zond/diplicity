d554cdc0875137258aeab46f0cf7b47d496ed457
= <=>
2024-06-02 23:22:18 +0100

    Add Ping/Pong Discord bot

--- app.go
+++ app.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/gorilla/mux"
 	"github.com/zond/diplicity/routes"
 	"google.golang.org/appengine/v2"
@@ -12,6 +16,31 @@ import (
 )
 
 func main() {
+
+	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+		if m.Content == "ping" {
+			s.ChannelMessageSend(m.ChannelID, "pong")
+		}
+	})
+
+	discord.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = discord.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer discord.Close()
+
+	fmt.Println("Discord bot is now running!")
+
 	jsonFormURL, err := url.Parse("/js/jsonform.js")
 	if err != nil {
 		panic(err)
