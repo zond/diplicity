03351811e69d29df2919cd74aa2e488cd4070e27
= <=>
2024-06-05 20:42:21 +0100

    Fix build script

--- .docker/docker-build.sh
+++ .docker/docker-build.sh
@@ -1,18 +1,19 @@
-# Description: Build docker image from Dockerfile
+#!/bin/bash
 
-Write-Host "Building Docker image..."
+# Description: Build docker image from Dockerfile
+echo "Building Docker image..."
 
 # Confirm that the script is being executed from the correct directory
-if (-not (Test-Path ".docker/docker-run.ps1")) {
-    Write-Host "You must run this command from the parent directory of this script."
-    exit
-}
+if [ ! -f ".docker/docker-run.sh" ]; then
+    echo "You must run this command from the parent directory of this script."
+    exit 1
+fi
 
-Write-Host "Confirmed that script is being executed from the correct directory."
+echo "Confirmed that script is being executed from the correct directory."
 
-$dockerCommand = "docker build --tag 'diplicity' ./.docker"
+dockerCommand="docker build --tag 'diplicity' ./.docker"
 
-Write-Host "Running Docker command: $dockerCommand"
+echo "Running Docker command: $dockerCommand"
 
 # Execute the Docker command
-Invoke-Expression $dockerCommand
+eval $dockerCommand
